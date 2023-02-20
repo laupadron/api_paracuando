6815c9096e43e2c6a47f25de815e821896e6fb26
laupadron <laupadron1458@gmail.com>
2023-02-20 19:22:36 +0100

    fix:correction created-updated migrations

--- database/migrations/20230220021502-create-publications-images.js
+++ database/migrations/20230220021502-create-publications-images.js
@@ -30,11 +30,11 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: false
       },
-      createdAt: {
+      created_at: {
         allowNull: false,
         type: Sequelize.DATE
       },
-      updatedAt: {
+      updated_at: {
         allowNull: false,
         type: Sequelize.DATE
       }
