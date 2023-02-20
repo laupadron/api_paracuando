c6eb3975475d8e9c24fc10cff242e0376e3899bc
Lauther Valladares <lautherharold27@gmail.com>
2023-02-19 19:12:20 -0500

    feat: documentation by laura

--- routes/models.router.js
+++ routes/models.router.js
@@ -1,6 +1,4 @@
 const express = require('express')
-const swaggerDocs = require('../swagger')
-
 // const routesUsers = require('./users.routes')
 
 // const isAuthenticatedByPassportJwt = require('../libs/passport')
@@ -8,13 +6,11 @@ const swaggerDocs = require('../swagger')
 const routesAuth = require('./auth.routes')
 const usersRoutes = require('./users.routes')
 
-function routerModels(app,PORT) {
+function routerModels(app) {
   const router = express.Router()
 
   app.use('/api/v1', router)
   router.use('/auth', routesAuth)
-  // router.use('/users',routesUsers)
-  swaggerDocs(router, PORT)
   router.use('/users', usersRoutes)
 }
 
