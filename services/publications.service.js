805388d2986bef4b2705610700b236aee25b317c
laupadron <laupadron1458@gmail.com>
2023-02-26 17:46:29 -0300

    feat:endpoints publications: add/delete vote and delete publication

--- services/publications.service.js
+++ services/publications.service.js
@@ -1,7 +1,7 @@
 const models = require('../database/models');
 const {v4: uuid4} = require('uuid');
 const  {CustomError}  = require('../utils/helpers');
-// const { hashPassword } = require('../libs/bcrypt');
+
 const { Op } = require('sequelize')
 
 
@@ -25,70 +25,44 @@ const { id } = query
 const publications = await models.Publications.scope('auth_flow').findAndCountAll(options)
 return publications
 }
-// async getVotes(query) {
-// 	const options = {
-// 	  where: {},
-// 	}
 
-//  const { user_id } = query
-//  if (user_id) {
-// 	options.where.user_id = { [Op.iLike]: `%${user_id}%` }
-//  }
-//  const { publications_id } = query
-//  if (publications_id) {
-// 	options.where.publications_id = { [Op.iLike]: `%${publications_id}%` }
-//  }
-//  options.distinct = true
-
-// const votes = await models.Votes.scope('auth_flow').findAndCountAll(options)
-// return votes
-// }
 
 	async delete (id){
 		const transaction = await models.sequelize.transaction();
 		try {
-			const publication = await models.Publications.findByPk(id)
-			if (!publication) throw new CustomError('Not found publication', 404, 'Not Found')
-			const deletePublication=await publication.destroy({transaction})
-			await transaction.commit()
-			 return deletePublication
+			const publication = await models.Publications.findByPk(id);
+			
+			if (!publication) throw new CustomError('Not found publication', 404, 'Not Found');
+			const deletePublication=await publication.destroy({transaction});
+			await transaction.commit();
+			 
+			return deletePublication;
 			
 		} catch (error) {
-			await transaction.rollback()
+			await transaction.rollback();
 			throw error;
 		}
 	};
 
-	async addAndDelete (publicationId,userId,publications_id){
+	async addAndDelete (publicationId,userId){
 		const transaction = await models.sequelize.transaction();
 		try {
-			const publication = await models.Publications.findByPk(publicationId)
+			// const publication = await models.Publications.findByPk(publicationId);
+			const vote =await models.Votes.findOne({where: {user_id: userId, publications_id: publicationId}});
+			if(vote){
+				const deleteVote = await models.Votes.destroy({where: { user_id: userId, publications_id: publicationId}}, {transaction});
+				await transaction.commit();
+				
+				return deleteVote;
 			
-			if(publication) {
-				const votes = await models.Votes.findByPk(publications_id)
-				console.log(votes)
-				if(publications_id === publicationId){
-					const deleteVote = await votes.delete()
-					await transaction.commit()
-					return deleteVote
-					
-				}
-				
-				
-		
-				//hacer otro if para que si encuentra la public en votes la borre o la agregue
-				// const addVote = await models.Votes.create({id: uuid4(),tag_id:idFromToken,publication_id:idParams});
-				
-				// return addVote
-				
 			}else{
-			 const newVote = await models.Votes.create({ user_id: userId, publications_id: publicationId}, {transaction})
-					console.log(newVote)
-					await transaction.commit()
-				
+					const newVote = await models.Votes.create({ user_id: userId, publications_id: publicationId}, {transaction});
+					await transaction.commit();
+					
+					return newVote;
 			}
 		} catch (error) {
-			await transaction.rollback()
+			await transaction.rollback();
 			throw error;
 		}
 	}
